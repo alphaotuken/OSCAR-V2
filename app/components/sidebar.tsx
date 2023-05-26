ef8243386f0d93d3fe1cc81b93526493d2eb9656
Talo <100890340+alphaotuken@users.noreply.github.com>
2023-05-26 19:46:50 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Oscar AI</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Your Personal ChatBot
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
